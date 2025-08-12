8f05c57ae55840ddb9007c38516b880a3c4d0880
Lucas Goldfarb <luhgoldfarb@gmail.com>
2025-08-12 18:17:18 -0400

    fix /widget

--- Source/app/__init__.py
+++ Source/app/__init__.py
@@ -1,38 +1,32 @@
-from flask import Flask, Response, redirect, url_for
+from flask import Flask, Response
 from app.modules.functions import make_link_page, make_svg_widget
 
-def _html_response(html: str) -> Response:
-    resp = Response(response=html, mimetype="text/html")
-    resp.headers["Cache-Control"] = "s-maxage=1"
-    resp.headers["Access-Control-Allow-Origin"] = "*"
-    return resp
+def _html(html: str) -> Response:
+    r = Response(html, mimetype="text/html")
+    r.headers["Cache-Control"] = "s-maxage=1"
+    r.headers["Access-Control-Allow-Origin"] = "*"
+    return r
 
-def _svg_response(svg: str) -> Response:
-    resp = Response(response=svg, mimetype="image/svg+xml")
-    resp.headers["Cache-Control"] = "s-maxage=1"
-    resp.headers["Access-Control-Allow-Origin"] = "*"
-    return resp
+def _svg(svg: str) -> Response:
+    r = Response(svg, mimetype="image/svg+xml")
+    r.headers["Cache-Control"] = "s-maxage=1"
+    r.headers["Access-Control-Allow-Origin"] = "*"
+    return r
 
 def create_app() -> Flask:
     app = Flask(__name__)
 
-    # Home -> playlist embed (HTML)
     @app.route("/")
     def home() -> Response:
-        return _html_response(make_link_page())
+        # show the playlist page
+        return _html(make_link_page())
 
-    # Keep the old /link path working
     @app.route("/link")
     def link() -> Response:
-        return _html_response(make_link_page())
+        return _html(make_link_page())
 
     @app.route("/widget")
     def widget() -> Response:
-        return _svg_response(make_svg_widget())
-
-    # If you still want a catch-all, send unknown paths to the playlist
-    @app.errorhandler(404)
-    def not_found(_):
-        return _html_response(make_link_page())
+        return _svg(make_svg_widget())
 
     return app
